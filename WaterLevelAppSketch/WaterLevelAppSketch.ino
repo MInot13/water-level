5aabe70290c0233ae0914f6438a960e10f0493e5
MInot13 <sunnymcvin@gmail.com>
2017-06-17 08:14:45 +0530

    Add files via upload

---
+++ WaterLevelAppSketch/WaterLevelAppSketch.ino
@@ -0,0 +1,33 @@
+/*
+ HC-SR04 Ping distance sensor]
+ VCC to arduino 5v GND to arduino GND
+ Echo to Arduino pin 12 Trig to Arduino pin 11
+ */
+
+#define trigPin 11
+#define echoPin 12
+int i=0;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin (9600);
+}
+
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  delay(500);
+  if(Serial.available() > 0){
+  i = Serial.read();
+  if(i==1){
+  distance=110-(distance-15);
+  
+  Serial.println(distance);
+  }
+  }
+}
